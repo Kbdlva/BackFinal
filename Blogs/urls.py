06e9b8a04af7c94cee2c7054f158d965a9576504
myen015 <210103009@stu.sdu.edu.kz>
2023-05-10 10:38:35 +0600

    first commit

--- Blogs/urls.py
+++ Blogs/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('home', views.home, name="home"),
     path('sign-up', views.sign_up, name="sign_up"),
     path('create-post', views.create_post, name="create_post"),
-    path('##', views.create_post, name="fake"),
 ]
