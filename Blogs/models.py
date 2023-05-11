25c5cd9ef51d14e728a873fb83d1abb63f88484d
guinnod <210103064@stu.sdu.edu.kz>
2023-05-11 18:05:02 +0600

    models

--- Blogs/models.py
+++ Blogs/models.py
@@ -1,14 +1,54 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class UserProfile(models.Model):
+    image = models.ImageField(null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    image = models.ImageField(null=True)
+    tag = models.ManyToManyField('Tag', related_name='tags')
     def __str__(self):
         return self.title + "\n" + self.description
 
     ##yernarrrrr
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    data = models.DateTimeField(auto_now=True)
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class SavedPost(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    post = models.ManyToManyField(Post, related_name='posts')
+
+class Chat(models.Model):
+    participants = models.ManyToManyField(User, related_name='chats')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    file = models.FileField()
+    timestamp = models.DateTimeField(auto_now_add=True)
